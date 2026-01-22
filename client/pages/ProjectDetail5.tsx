e53d38dc0675f6719a4e1242dbe4af2dd92cf409
MushLaptop <kennethSk@yandex.ru>
2026-01-23 01:13:00 +0300

    me gif fix

--- client/pages/ProjectDetail5.tsx
+++ client/pages/ProjectDetail5.tsx
@@ -258,7 +258,7 @@ export default function ProjectDetail5() {
           </div>
 
           {/* Section: Rotated portrait + Text */}
-          <div className="flex flex-col lg:flex-row justify-between items-center gap-8 lg:gap-12 w-full px-4 lg:pr-[180px] h-auto lg:h-[936px]">
+          <div className="flex flex-col lg:flex-row justify-between items-center gap-8 lg:gap-12 w-full lg:pr-[180px] h-auto lg:h-[936px]">
             <div className="relative w-full lg:w-auto h-[600px] lg:h-full flex items-center justify-center">
               <img
               src="/ProjectDetail5/me.gif"
