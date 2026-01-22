f555a1ff2ca973d01c70afd60b20cadd57e93a51
MushLaptop <kennethSk@yandex.ru>
2026-01-23 01:47:21 +0300

    fix guys2

--- client/pages/ProjectDetail5.tsx
+++ client/pages/ProjectDetail5.tsx
@@ -187,8 +187,8 @@ export default function ProjectDetail5() {
           </div>
 
           {/* Section: Large image + Text */}
-          <div className="flex flex-col lg:flex-row justify-between items-end gap-8 lg:gap-12 w-full px-4 lg:pr-[180px]">
-            <div className="w-full lg:relative lg:left-1/2 lg:right-1/2 lg:-translate-x-1/2 lg:w-screen">
+          <div className="flex flex-col lg:flex-row justify-between items-end gap-8 lg:gap-12 w-full lg:pr-[180px]">
+            <div className="w-full lg:relative lg:left-0 lg:w-screen lg:-ml-4">
               <BlurUpImage
                 src="/ProjectDetail5/guys2.png"
                 thumb="/thumbs/ProjectDetail5/guys2.png"
