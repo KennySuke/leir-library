4f51835fa623d66df8a2e6b3c5604db038d997bd
MushLaptop <kennethSk@yandex.ru>
2026-01-23 00:39:20 +0300

    photos fix

--- client/pages/ProjectDetail5.tsx
+++ client/pages/ProjectDetail5.tsx
@@ -121,7 +121,7 @@ export default function ProjectDetail5() {
           </div>
 
           {/* Section: Lab image + Text + Rotated images */}
-          <div className="flex flex-col lg:flex-row justify-between items-center gap-8 lg:gap-12 w-full px-4">
+          <div className="flex flex-col lg:flex-row justify-between items-start gap-8 lg:gap-12 w-full px-4">
             {/* Left lab image */}
             <BlurUpImage
               src="/ProjectDetail5/lab1.png"
