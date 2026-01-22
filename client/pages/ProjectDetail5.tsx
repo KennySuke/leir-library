7bbb0f687ef4a3841c855e6a7c31609fa8e5727d
MushLaptop <kennethSk@yandex.ru>
2026-01-23 00:15:50 +0300

    photos fix down

--- client/pages/ProjectDetail5.tsx
+++ client/pages/ProjectDetail5.tsx
@@ -299,16 +299,16 @@ export default function ProjectDetail5() {
                   Это мармелад, который невозможно попробовать на вкус, но
                   который невозможно забыть.
                 </span>
-              </div>
+            </div>
           
             <div className="w-full lg:w-auto lg:absolute lg:right-0 lg:top-[133px]">
-                <BlurUpImage
-                  src="/ProjectDetail5/guys.png"
-                  thumb="/thumbs/ProjectDetail5/guys.png"
-                  alt="Artists group final"
-                  className="w-full lg:w-[806px] aspect-[62/59] object-cover"
-                />
-              </div>
+              <BlurUpImage
+                src="/ProjectDetail5/guys.png"
+                thumb="/thumbs/ProjectDetail5/guys.png"
+                alt="Artists group final"
+                className="w-full lg:w-[806px] aspect-[62/59] object-cover"
+              />
+            </div>
           </div>
 
         </div>
